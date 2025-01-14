baeeab34c4c1b5ff30771fd389466c30d84b92d6
Beatriz <beatriznovais1404@gmail.com>
2025-01-14 14:24:29 -0300

    Crud da classe tema finalizado

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -4,10 +4,13 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -31,31 +34,48 @@ public class Postagem {
 	@UpdateTimestamp
 	private LocalDateTime data;
 	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
 	public Long getId() {
 		return id;
 	}
+	
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
 	public String getTitulo() {
 		return titulo;
 	}
+	
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+	
 	public String getTexto() {
 		return texto;
 	}
+	
 	public void setTexto(String texto) {
 		this.texto = texto;
 	}
+	
 	public LocalDateTime getData() {
 		return data;
 	}
+	
 	public void setData(LocalDateTime data) {
 		this.data = data;
 	}
 	
+	public Tema getTema() {
+		return tema;
+	}
 	
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 
 }
